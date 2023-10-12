d68cf5c6a4117cad838c1ec9b319cb3420b9f17b
andrei-pvl <147505074+andrei-pvl@users.noreply.github.com>
2023-10-12 21:47:53 +0300

    Update IMoney.cs

--- IMoney.cs
+++ IMoney.cs
@@ -1,20 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoneySum
+namespace MoneySumm
 {
     public interface IMoney
     {
         double Sum { get; set; }
         string CodVal { get; set; }
         double CursUsd { get; set; }
-
-        bool Equals(Money money);
-        double SumOfSum(Money money1, Money money2);
-        double Calculate(Money money);
-        string ToString();
+   
+        Money Add(Money money1);
+        Money Calculate(Money money);        
     }
 }
